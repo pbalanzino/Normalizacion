cd8c35f36065c83628da0824e1cdc4a15160bc9e
Pablo Ezequiel Balanzino <pbalanzino@me.com>
2022-03-31 18:56:27 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import __dirname from './utils.js';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname+'/public'));
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+const io = new Server(server);
+const log = [];
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
