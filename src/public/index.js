fd63c6ed53f9c18b659d2477bd093bc825045dee
Pablo Ezequiel Balanzino <pbalanzino@me.com>
2022-03-31 19:12:02 -0300

    Implemente SweetAlert

--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,14 @@
 let socket = io();
+
+let chatBox = document.getElementById('chatBox');
+let user;
+Swal.fire({
+    title: 'Enter your name',
+    input: 'text',
+    allowOutsideClick: false,
+    inputValidator: (value) => {
+        if (!value) {
+            return 'You need to write something!'
+        }
+    }
+})
