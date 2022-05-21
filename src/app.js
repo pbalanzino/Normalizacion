ddca9aa5ea7f26adc474e33eefc88fea30b6f18c
Pablo Ezequiel Balanzino <pbalanzino@me.com>
2022-05-21 19:51:25 -0300

    Normalizr

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
-import express from 'express';
-import { Server } from 'socket.io';
-import __dirname from './utils.js';
+/** @format */
+
+import express from "express";
+import { Server } from "socket.io";
+import __dirname from "./utils.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.use(express.static(__dirname+'/public'));
-
+app.use(express.static(__dirname + "/public"));
 const server = app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port http://localhost:${PORT}`);
 });
 const io = new Server(server);
 const log = [];
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
+io.on("connection", (socket) => {
+    console.log("Client connected");
+    socket.on("message", (data) => {
+        log.push(data);
+        io.emit("log", log);
+    });
 });
--- src/public/index.js
+++ src/public/index.js
@@ -2,8 +2,12 @@ let socket = io();
 
 let chatBox = document.getElementById('chatBox');
 let user;
+let log = document.getElementById('log');
+
+/* Alert de identificacion */
+
 Swal.fire({
-    title: 'Enter your name',
+    title: 'Ingresa tu nombre',
     input: 'text',
     allowOutsideClick: false,
     inputValidator: (value) => {
@@ -11,4 +15,29 @@ Swal.fire({
             return 'You need to write something!'
         }
     }
-})
+}).then(result => {
+    if (result.value) {
+        user = result.value;
+        socket.emit('new-user', user);
+    }
+});
+
+chatBox.addEventListener('keyup', (e) => {
+    if (e.key == 'Enter') {
+        if(chatBox.value.trim().length > 0){
+        socket.emit('message', {user, message: chatBox.value.trim()});
+        chatBox.value = '';
+        }
+    }
+});
+
+/* Sockets */
+
+socket.on('log', data => {
+    let message = '';
+    data.forEach(log => {
+        message += `<p><strong>${log.user}</strong> dice: ${log.message}</p>`;
+        });
+    log.innerHTML = message;
+    }
+);
